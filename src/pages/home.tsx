bc7ec01c802adfc4c526124fe60fd160424b6f6b
lee-1980 <ljsitsolution@gmail.com>
2023-10-17 18:00:16 +0800

    add two additional stock sync runners

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -25,6 +25,10 @@ const Home = () => {
     const [stockMessage, setStockMessage] = useState<string>('Real Time Notification, Don\'t refresh the page while the system is running once click the Force Sync button');
     const [orderMessage, setOrderMessage] = useState<string>('Real Time Notification, Don\'t refresh the page while the system is running once click the Force Sync button');
     const [stockTimer, setStockTimer] = useState<Dayjs | null>(dayjs('00:00:01', 'HH:mm:ss'));
+    const [stockATimer, setStockATimer] = useState<Dayjs | null>(dayjs('00:00:01', 'HH:mm:ss'));
+    const [stockATimerChecked, setStockATimerChecked] = useState(false);
+    const [stockBTimer, setStockBTimer] = useState<Dayjs | null>(dayjs('00:00:01', 'HH:mm:ss'));
+    const [stockBTimerChecked, setStockBTimerChecked] = useState(false);
     const [lastSync, setLastSync] = useState<Dayjs | null>(dayjs('1970-01-01 00:00:01', 'YYYY-MM-DD HH:mm:ss'));
 
     React.useEffect(() => {
@@ -32,11 +36,15 @@ const Home = () => {
 
         apiWrapper('home', null)
             .then(res => res.json())
-            .then(  ({febestCount, autoplusCount, serverOn, runTime, lastSyncTime, StockTime}) => {
+            .then(  ({febestCount, autoplusCount, serverOn, runTime, lastSyncTime, StockTime, StockATime, StockBTime, stockAOn, stockBOn}) => {
                 setLastSync(dayjs(lastSyncTime, 'YYYY-MM-DD HH:mm:ss'));
                 setOrderTimer(dayjs(runTime, 'HH:mm:ss'));
                 setStockTimer(dayjs(StockTime, 'HH:mm:ss'));
+                setStockATimer(dayjs(StockATime, 'HH:mm:ss'));
+                setStockBTimer(dayjs(StockBTime, 'HH:mm:ss'));
                 setChecked(serverOn);
+                setStockATimerChecked(stockAOn);
+                setStockBTimerChecked(stockBOn);
                 setFebestCount(febestCount);
                 setAutoplusCount(autoplusCount);
                 setIsLoading(false);
@@ -68,17 +76,35 @@ const Home = () => {
 
     const switchHandler = async (event : React.ChangeEvent<HTMLInputElement>) => {
         setIsLoading(true);
+        let response;
 
-        console.log(!event.target.checked)
-        const response = await updateSetting('systemOnOff', event.target.checked? 'true': 'false');
-
-        // after updating the database, update the state
-
-        if (response.status === 200) {
-            setChecked(!event.target.checked)
-        } else {
-            return Promise.reject();
+        if(event.target.id && event.target.id === 'stockA') {
+            response = await updateSetting('stockTimeAOnOff', event.target.checked? 'true': 'false');
+            if (response.status === 200) {
+                setStockATimerChecked(!event.target.checked)
+            } else {
+                return Promise.reject();
+            }
         }
+        else if (event.target.id && event.target.id === 'stockB') {
+            response = await updateSetting('stockTimeBOnOff', event.target.checked? 'true': 'false');
+            if (response.status === 200) {
+                setStockBTimerChecked(!event.target.checked)
+            } else {
+                return Promise.reject();
+            }
+        }
+        else{
+            response = await updateSetting('systemOnOff', event.target.checked? 'true': 'false');
+            // after updating the database, update the state
+
+            if (response.status === 200) {
+                setChecked(!event.target.checked)
+            } else {
+                return Promise.reject();
+            }
+        }
+
         setIsLoading(false);
     };
 
@@ -95,16 +121,39 @@ const Home = () => {
         setIsLoading(false);
     };
 
-    const onStockTimeChange = async (time: Dayjs | null, timeString: string) => {
+    const onStockTimeChange = async (time: Dayjs | null, timeString: string, id: string) => {
         setIsLoading(true);
+        let response;
+        console.log(id, "event");
 
-        const response = await updateSetting('stockTime', timeString);
-        // after updating the database, update the state
-        if (response.status === 200) {
-            setStockTimer(time)
-        } else {
-            return Promise.reject();
+        if(id === 's_timea') {
+            response = await updateSetting('stockTimeA', timeString);
+            // after updating the database, update the state
+            if (response.status === 200) {
+                setStockATimer(time)
+            } else {
+                return Promise.reject();
+            }
         }
+        else if( id === 's_timeb') {
+            response = await updateSetting('stockTimeB', timeString);
+            // after updating the database, update the state
+            if (response.status === 200) {
+                setStockBTimer(time)
+            } else {
+                return Promise.reject();
+            }
+        }
+        else{
+            response = await updateSetting('stockTime', timeString);
+            // after updating the database, update the state
+            if (response.status === 200) {
+                setStockTimer(time)
+            } else {
+                return Promise.reject();
+            }
+        }
+
         setIsLoading(false);
     }
 
@@ -200,6 +249,7 @@ const Home = () => {
                             <Typography fontSize={18} fontWeight={600} color="#11142d">
                                 Last eBay Order Sync Time
                             </Typography>
+
                             <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
                                 <DatePicker
                                     format="YYYY-MM-DD HH:mm:ss"
@@ -211,8 +261,6 @@ const Home = () => {
                                 <p style={{ color: "#fff" , backgroundColor: "#1677ff", borderRadius: "5px", padding: "10px 15px"}}>This value must not be later than the current time. This value is updated regularly when the "posting orders to datapel" job runs.</p>
                             </Stack>
                         </Stack>
-
-
                     </Box>
                     <Box
                         p={4}
@@ -230,15 +278,48 @@ const Home = () => {
                             </Typography>
 
                             <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
-                                <TimePicker allowClear={false} use12Hours={false} value={stockTimer} format="HH:mm:ss" onChange={onStockTimeChange}/>
-                                <Button variant="contained" color="primary" onClick={() => onStockTimeChange(dayjs().add(5, 'second'), dayjs().add(5, 'second').format('HH:mm:ss'))}>Force Run in 5 seconds</Button>
-                            </Stack>
-
-                            <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
                                 <p style={{ color: "#fff" , backgroundColor: "#7c7eff", borderRadius: "5px", padding: "10px 15px", border: "2px double #cbb1ff"}}>
-                                {stockMessage}
+                                    {stockMessage}
                                 </p>
                             </Stack>
+
+                            <Box p={1} m={1} component="fieldset" border={"2px solid #7c7eff"} borderRadius="5px">
+                                <legend style={{color: "#7c7eff", fontWeight: "600"}}>Main Stock Sync</legend>
+                                <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
+                                    <TimePicker id={"s_time"} allowClear={false} use12Hours={false} value={stockTimer}
+                                                format="HH:mm:ss" onChange={(date, dateString) => onStockTimeChange(date, dateString, "s_time")}/>
+                                    <Button variant="contained" color="primary"
+                                            onClick={() => onStockTimeChange(dayjs().add(5, 'second'), dayjs().add(5, 'second').format('HH:mm:ss'), "s_time")}>Force
+                                        Run in 5 seconds</Button>
+                                </Stack>
+                            </Box>
+
+                            <p style={{ color: "#fff" , backgroundColor: "#1677ff", borderRadius: "5px", padding: "8px 14px"}}>It is recommended to stagger stock change runners by a time difference of more than 4 hours.</p>
+
+                            <Stack direction="row" gap={4} >
+                                <Box p={1} m={1} component="fieldset" border={"2px solid #7c7eff"} borderRadius="5px">
+                                    <legend style={{color: "#7c7eff", fontWeight: "600"}}>
+                                        Stock Sync A
+                                        <Switch id={"stockA"} color="primary"  checked={stockATimerChecked} onChange={switchHandler}/>
+                                    </legend>
+                                    <Stack my="20px" direction="row" gap={4} flexWrap="wrap" style={{opacity: stockATimerChecked?"1":"0.2"}}>
+                                        <TimePicker id={"s_timea"} allowClear={false} use12Hours={false} value={stockATimer}
+                                                    format="HH:mm:ss" onChange={(date, dateString) => onStockTimeChange(date, dateString, "s_timea")}/>
+                                    </Stack>
+                                </Box>
+
+                                <Box p={1} m={1} component="fieldset" border={"2px solid #7c7eff"} borderRadius="5px">
+                                    <legend style={{color: "#7c7eff", fontWeight: "600"}}>
+                                        Stock Sync B
+                                        <Switch id={"stockB"} color="primary" checked={stockBTimerChecked} onChange={switchHandler}/>
+                                    </legend>
+                                    <Stack my="20px" direction="row" gap={4} flexWrap="wrap" style={{opacity: stockBTimerChecked?"1":"0.2"}}>
+                                        <TimePicker id={"s_timeb"} allowClear={false} use12Hours={false} value={stockBTimer}
+                                                    format="HH:mm:ss" onChange={(date, dateString) => onStockTimeChange(date, dateString, "s_timeb")}/>
+                                    </Stack>
+                                </Box>
+                            </Stack>
+
                         </Stack>
                     </Box>
                 </Stack>
